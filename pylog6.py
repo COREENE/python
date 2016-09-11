3aeab7f51e088ba5324add51aa9e07a60d022f03
Coreene Wong <justmypace@163.com>
2016-09-11 23:10:57 +0800

    aiohttp

--- pylog6.py
+++ pylog6.py
@@ -337,16 +337,17 @@ async def index(request):
     return web.Response(body=b'<h1>Index</h1>')
 
 async def hello(request):
-    await asyncio.sleep(0.5):
+    await asyncio.sleep(0.5)
     text='<h1>hello,%s</h>'%request.match_info['name']
     return web.Response(body=text.encode('utf-8'))
 
 async def init(loop): #初始化函数init()也是一个coroutine
     app=web.Application(loop=loop) 
-    # loop=loop: name=value (loop parameter constructor is keyword-only )
+    # loop=loop: key=value (loop parameter constructor is keyword-only )
     app.router.add_route('GET','/',index)
+    # method:GET path:/index 调用index(request)处理
     app.router.add_route('GET','/hello/{name}',hello)
-    srv=await loop.creat_server(app.make_handler(),'127.0.0.1',8000)
+    srv=await loop.create_server(app.make_handler(),'127.0.0.1',8000)
     #loop.creat_server()利用asyncio创建TCP服务。
     print('Server staarted at http://127.0.0.1:8000...')
     return srv
